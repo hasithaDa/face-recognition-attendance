aef418f339b37ed1ce2d844d94bdea04c32b7844
hasithaDa <hamdhasith@gmail.com>
2025-02-25 12:37:22 +0530

    Urls file updated

--- attendance_system_facial_recognition/urls.py
+++ attendance_system_facial_recognition/urls.py
@@ -1,6 +1,4 @@
-"""attendance_system_facial_recognition URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
+"""The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
